4ae94bb961c03758ce7bc63df8421cd09ef29f30
JakubKonkol <s24406@pjwstk.edu.pl>
2023-12-18 09:27:08 +0100

    AuthProvider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import {useEffect, useState} from "react";
 import usersJSON from './users.json';
 import UserList from "./UserList";
 import Filter from "./Filter";
+import {useAuth} from "./AuthProvider";
 
 function App() {;
+    const { login } = useAuth();
     const [users, setUsers] = useState([]);
     const [filteredUsers, setFilteredUsers] = useState([]);
     useEffect(() => {
         setUsers(usersJSON.users);
         setFilteredUsers(usersJSON.users)
-    }, []);
+        login(usersJSON.users[0])
+    }, [login]);
   return (
         <div className="App">
           <h1> Kolokwium</h1>
